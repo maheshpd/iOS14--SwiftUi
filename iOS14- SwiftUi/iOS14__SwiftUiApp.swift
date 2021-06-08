3639533c9932bad0ceb3c3c740086b0c36cda809
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-06-08 10:13:20 +0530

    Initial Commit

---
+++ iOS14- SwiftUi/iOS14__SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS14__SwiftUiApp.swift
+//  iOS14- SwiftUi
+//
+//  Created by Mahesh Prasad on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS14__SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
