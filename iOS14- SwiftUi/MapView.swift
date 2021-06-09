1253ceb79eca1eee0a1784c6ee7c66f509dfa917
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-06-09 10:43:58 +0530

    add MapView

---
+++ iOS14- SwiftUi/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  iOS14- SwiftUi
+//
+//  Created by Mahesh Prasad on 08/06/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
